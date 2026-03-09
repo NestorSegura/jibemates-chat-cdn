35a239e8d3cb9ba3bcff169f7400a845b9da67c9
nestorsegura <nestor.segura@gmail.com>
2026-03-09 15:45:40 +0100

    Deploy jibemates-chat-CdoKV9pv.iife.js

---
+++ loader.js
@@ -0,0 +1,5 @@
+(function(){
+  var s=document.createElement('script');
+  s.src='https://cdn.jsdelivr.net/gh/NestorSegura/jibemates-chat-cdn@main/jibemates-chat-CdoKV9pv.iife.js';
+  document.head.appendChild(s);
+})();
